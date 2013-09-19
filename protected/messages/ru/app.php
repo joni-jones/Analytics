8656e0ae5678873d42c6850a2542c881f7a361c0
joni-jones <jonik.lowkick@gmail.com>
2013-09-19 17:27:09 +0300

    Added localization messages

---
+++ protected/messages/ru/app.php
@@ -0,0 +1,10 @@
+<?php
+return array(
+    'Email' => 'Email',
+    'Password' => 'Пароль',
+    'Remember Me' => 'Запомнить Меня',
+    'Login' => 'Вход',
+    'Logout' => 'Выход',
+    'Please login with your email and password below' => 'Пожалуйса используйте для входа ваш email и пароль',
+    'Forgot your password' => 'Забыли свой пароль',
+);
---
+++ protected/messages/ru/sys.php
@@ -0,0 +1,4 @@
+<?php
+return array(
+    'Incorrect email or password.' => 'Неправильный email или пароль.',
+);
