a82e38213ad77eea5d4ba08b80d2f69eb3a631cd
joni-jones <jonik.lowkick@gmail.com>
2013-09-23 11:43:58 +0300

    Added progress bar view

---
+++ themes/classic/views/layouts/_blocks/progress_bar.php
@@ -0,0 +1,5 @@
+<div id="progress-modal" class="modal progress-modal hide">
+    <div class="progress progress-striped active">
+        <div class="bar" style="width: 100%;"></div>
+    </div>
+</div>
