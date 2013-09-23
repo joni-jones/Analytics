b03362dd37b577176b766664671015f21fbdda6e
joni-jones <jonik.lowkick@gmail.com>
2013-09-23 12:02:53 +0300

    Updated default error view

--- themes/classic/views/system/error.php
+++ themes/classic/views/system/error.php
@@ -1,6 +1,29 @@
 <?php
 $this->pageTitle = Yii::app()->name.' | '.$code;
 ?>
-<div class="row error-container">
-    <h2><?=$code.' '.CHtml::encode($message);?></h2>
+<div class="row error-page">
+    <div class="modal show">
+        <div class="modal-dialog">
+            <div class="modal-content">
+                <div class="modal-body">
+                    <div class="container">
+                        <div class="col-md-6">
+                            <div class="error-code"><?=$code;?></div>
+                            <div class="error-message"><?=CHtml::encode($message);?></div>
+                        </div>
+                        <div class="col-md-6">
+                            <p><?=Yii::t('sys', 'Please, try again later');?>.</p>
+                            <p>
+                                <?=Yii::t('app', 'Or contact with our administrator');?><br/>
+                                <a href="mailto:<?=Yii::app()->params['adminEmail'];?>">
+                                    <span class="glyphicon glyphicon-envelope"></span>
+                                    <?=Yii::t('app', 'Contact');?>
+                                </a>
+                            </p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
 </div>
